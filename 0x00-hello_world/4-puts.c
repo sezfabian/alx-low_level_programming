c90a069cd961fea4c71d9fe7680ae9ae587b2b9d
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-14 16:27:44 +0300

    Update 4-puts.c
    FInalls

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
-#include<string.h>
 
 /*
  * main - Entry point
  * Return: Always 0 (Success)
- * This is a c program using puts function
+ * 
  */
 int main(void)
 {
 	char st1[] = "\"Programming is like building a multilingual puzzle";
-puts(st1);
-return (0);
+	puts(st1);
+	return (0);
 }
