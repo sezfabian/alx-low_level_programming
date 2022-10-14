5acdef19c5cadba9873c6b140a8cb6216057c64c
sezfabian <cheruiyotfabian@gmail.com>
2022-10-14 06:21:55 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+
+/*
+ * This is a c program using puts function
+ */
+int main(void)
+{
+	char st1[] = "\"Programming is like building a multilingual puzzle";
+puts(st1);
+return (0);
+}
