bcfc00bc1af53163ebe91ee15c2a28d36a1a2361
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-17 01:32:22 +0300

    Create 2-print_alphabet.c
    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ *main - Entry point of program
+ *
+ *Return: Always 0 - successful
+ */
+ int main(void)
+ {
+   char str[] = "abcdefghijklmnopqrstuvwxyz";
+   int num;
+   
+   for( num = 0; num < 27 ; num++)
+   {
+     putchar(str[num]);
+   }
+   
+   putchar("\n");
+   return 0;
+ }
