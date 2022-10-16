4b4ecd0d6dae7badd2e8d862efee0b7af88f97a7
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-17 01:59:02 +0300

    Create 3-print_alphabets.c
    alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/**
+ *main - Entry point of program
+ *
+ *Return: Always 0 - successful
+ */
+int main(void)
+{
+char str1[] = "abcdefghijklmnopqrstuvwxyz\n";
+char str1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ\n";
+int num1;
+int num2;
+
+for (num1 = 0; num1 <= 26 ; num1++)
+{
+putchar(str[num]);
+}
+  
+for (num2 = 0; num2 <= 26 ; num2++)
+{
+putchar(str[num]);
+}
+
+return (0);
+}
