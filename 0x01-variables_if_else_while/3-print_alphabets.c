bdd8d01db192e4dfd16e1b56cdd1308d60e222d1
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-17 02:00:04 +0300

    Update 3-print_alphabets.c
    corrections

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,18 +8,18 @@
 int main(void)
 {
 char str1[] = "abcdefghijklmnopqrstuvwxyz\n";
-char str1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ\n";
+char str2[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ\n";
 int num1;
 int num2;
 
 for (num1 = 0; num1 <= 26 ; num1++)
 {
-putchar(str[num]);
+putchar(str1[num]);
 }
   
 for (num2 = 0; num2 <= 26 ; num2++)
 {
-putchar(str[num]);
+putchar(str2[num]);
 }
 
 return (0);
