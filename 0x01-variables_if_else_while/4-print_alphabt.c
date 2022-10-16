3780ac0bc12d36b4a0e8b99c7e40d98b8f0e52f0
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-17 02:07:37 +0300

    Create 4-print_alphabt.c
    exempt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ *main - Entry point of program
+ *
+ *Return: Always 0 - successful
+ */
+int main(void)
+{
+char str[] = "abcdefghijklmnopqrstuvwxyz\n";
+int num;
+
+for (num = 0; num <= 26 ; num++)
+{
+if(str[num] = q || str[num] = e)
+continue;
+putchar(str[num]);
+}
+
+return (0);
+}
