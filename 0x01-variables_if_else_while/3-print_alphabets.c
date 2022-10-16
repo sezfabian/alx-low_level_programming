c17d2386f03a78c5051ea5477042857156e11425
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-17 02:02:04 +0300

    Update 3-print_alphabets.c
    nice

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-char str1[] = "abcdefghijklmnopqrstuvwxyz\n";
+char str1[] = "abcdefghijklmnopqrstuvwxyz";
 char str2[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ\n";
 int num1;
 int num2;
 
-for (num1 = 0; num1 <= 26 ; num1++)
+for (num1 = 0; num1 <= 25 ; num1++)
 {
 putchar(str1[num1]);
 }
-  
+
 for (num2 = 0; num2 <= 26 ; num2++)
 {
 putchar(str2[num2]);
