fbf4e73903d5bc7f62bf364ada0662b78e7ae8ff
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-17 04:26:54 +0300

    Update 100-print_comb3.c
    less

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,12 +10,14 @@ int main(void)
 int num = 0;
 int num2;
 
-for ( num = num; num <=8 ; num++)
+for (num = num; num <= 8 ; num++)
 {
-for ( num2 = num+1; num2 <=9 ; num2++)
+for (num2 = num + 1; num2 <= 9 ; num2++)
 {
 putchar(num % 10 + '0');
 putchar(num2 % 10 + '0');
+if (num == 8 || num2 == 9)
+continue;
 putchar(',');
 putchar(' ');
 }
