c60c57566a8c45deb192f171e30515762ffd6e59
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-17 03:10:22 +0300

    Create 7-print_tebahpla.c
    putchar rev

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+include<stdio.h>
+
+/**
+ *main - Entry point of program
+ *
+ *Return: Always 0 - successful
+ */
+int main(void)
+{
+char str[] = "\nabcdefghijklmnopqrstuvwxyz";
+int num;
+
+for (num = 26; num >= 0 ; num--)
+{
+putchar(str[num]);
+}
+
+return (0);
+}
