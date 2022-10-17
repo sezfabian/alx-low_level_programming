565df7f536fd4b06f32cf5b628783aa7cda1b864
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-17 22:49:09 +0300

    Update 102-print_comb5.c
    0100

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,10 +20,12 @@ for (num2 = num; num2 <= 9 ; num2++)
 {
 for (num3 = num1 + 1; num3 <= 9 ; num3++)
 {
+if (num == 0 && num1 == 1 && num2 == 0 && num3 ==0)
+continue;
 putchar(num % 10 + '0');
 putchar(num1 % 10 + '0');
 putchar(' ');
-putchar(num2% 10 + '0');
+putchar(num2 % 10 + '0');
 putchar(num3 % 10 + '0');
 putchar(',');
 putchar(' ');
