b0a18bdd4b51eeb7d9a5c2427d80de668afa3046
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-17 04:22:39 +0300

    Create 100-print_comb3.c
    100 print

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+ *main - Entry point of program
+ *
+ *Return: Always 0 - successful
+ */
+int main(void)
+{
+int num = 0;
+int num2;
+
+for ( num = num; num <=8 ; num++)
+{
+for ( num2 = num+1; num2 <=9 ; num2++)
+{
+putchar(num % 10 + '0');
+putchar(num2 % 10 + '0');
+putchar(',');
+putchar(' ');
+}
+}
+
+putchar('\n');
+
+return (0);
+}
