97bebf12aacafc3dc9ec38aef1eb6ebb0240aae3
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-17 04:36:34 +0300

    Create 9-print_comb.c
    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ *main - Entry point of program
+ *
+ *Return: Always 0 - successful
+ */
+int main(void)
+{
+int num = 0;
+int num2;
+
+for (num = num; num <= 9 ; num++)
+{
+putchar(num % 10 + '0');
+if (num == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+
+putchar('\n');
+
+return (0);
+}
