2fc4a57c2092d49290a68ad984c1fb2e1c59af4b
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-19 00:45:32 +0300

    Update 3-islower.c
    betty

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,14 +3,16 @@
 /**
  * _islower - function checks lower case
  *
+ * c - character to be checked
  * Return: 1 - lower case, 0 - not lower case.
  */
 int _islower(int c)
 {
-if (c >= 'a' && c <= 'z' )
+if (c >= 'a' && c <= 'z')
 {
 return (1);
-}else
+}
+else
 {
 return (0);
 }
