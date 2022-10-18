9f7e25216cf3c8fa814dc4ded60c35373a7dfa58
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-19 00:05:11 +0300

    Update 1-alphabet.c
    bettty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 
 /**
- * main - check the code
+ * print_alphabet - prints the alphabet in lower case.
  *
  * Return: Always 0 success.
  */
 void print_alphabet(void)
 {
-char str[]="abcdefghijklmnopqrstuvwxyz\n";
+char str[] = "abcdefghijklmnopqrstuvwxyz\n";
 int n;
 
-for(n = 0; n <= 26; n++)
+for (n = 0; n <= 26; n++)
 {
 putchar(str[n]);
 }
