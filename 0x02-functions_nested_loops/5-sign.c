1894ea5e75297a447789b162a9ada4a8fbb94b65
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-19 01:27:15 +0300

    Update 5-sign.c
    putt

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include "main.h"
 
 /**
  * print_sign - function to print number sign
@@ -10,17 +11,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-putchar('+');
+_putchar('+');
 return (1);
 }
 else if (n == 0)
 {
-putchar('0');
+_putchar('0');
 return (0);
 }
 else
 {
-putchar('-');
+_putchar('-');
 return (-1);
 }
 }
