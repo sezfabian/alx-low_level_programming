ab3bc8392849797346e1e02b140bf5fd37692f61
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-19 04:11:55 +0300

    Create 9-times_table.c
    lets see

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+ * times_table - function prints  9 times table
+ *
+ * Return: 0 always success.
+ */
+void times_table(void)
+{
+int num1;
+int num2;
+
+for (num1=0; num1 <= 9; num1++)
+{
+for (num2 = 0; num2 <= 9; num2++)
+{
+if (num2 > 0 && (num1 * num2) < 10)
+{
+putchar(' ');
+}
+printf( "%i", (num1 * num2));
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+}
+}
