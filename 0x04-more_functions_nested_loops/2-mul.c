00ddcbf453d5d65befc15a37abe9b697d48a2fa7
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-24 00:01:26 +0300

    Create 2-mul.c
    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+include<stdio.h>
+
+/**
+ * mul - finds multiple of two integers
+ *
+ * @a : integer one
+ * @b : integer two
+ *
+ * Return: 1 for digit, 0 otherwise.
+ */
+
+int mul(int a, int b)
+{
+int m;
+m = a * b;
+
+return (m);
+}
