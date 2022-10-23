6c7f9f51f68eb932893dbc35bbaa397f53e0ba12
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-23 23:29:48 +0300

    Create 1-isdigit.c
    digit check

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * _isdigit - checks for digit integers
+ *
+ * @c : integer to be checked
+ *
+ * Return: 1 for digit, 0 otherwise.
+ */
+
+int _isdigit(int c)
+{
+int n;
+int b = 0;
+  
+for (n = 0; n <= 9; ++n)
+{
+if (c == n)
+{
+b = 1;
+}
+}
+  
+return (b);
+}
