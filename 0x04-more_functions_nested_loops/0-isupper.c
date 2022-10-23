e1ca22d0b7e850f670ee8634764053d00fa313d1
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-23 23:07:10 +0300

    Update 0-isupper.c
    betty

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,17 +7,16 @@
  *
  * Return: 1 for uppercase, 0 otherwise.
  */
-
 int _isupper(int c)
 {
-      char A;
-      int b = 0;
-      for (A = 'A'; A <= 'Z'; ++A)
-      {
-            if (c == A)
-            {
-                  b = 1;
-            }
-      }
-      return (b);
+char A;
+int b = 0;
+for (A = 'A'; A <= 'Z'; ++A)
+{
+if (c == A)
+{
+b = 1;
+}
+}
+return (b);
 }
