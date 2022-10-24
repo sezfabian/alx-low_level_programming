5c921f52959a2f3e1e763eda56012ba3c2f5b3a3
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-24 18:25:46 +0300

    Update 9-fizz_buzz.c
    again

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,28 +2,32 @@
 
 /**
  * main - Entry point of the program
- *
  */
 int main(void)
 {
 int n;
+
 for(n = 1; n <= 100; ++n)
 {
-if (n % 3 == 0 && n % 5 == 0 )
-printf( "FizzBuzz ");
+if (n % 3 == 0 && n % 5 == 0)
+{
+printf("FizzBuzz");
+}
 else if (n % 3 == 0)
 {
-printf("Fizz ");
+printf("Fizz");
 }
 else if (n % 5 == 0)
 {
-printf("Buzz ");
+printf("Buzz");
 }
 else
 {
-printf("%d ", n);
+printf("%d", n);
 }
+putchar(' ');
 }
 printf("\n");
+
 return (0);
 }
