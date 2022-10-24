4320907e65f0076b7106b68f10e6632869050724
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-24 10:19:40 +0300

    Create 6-print_line.c
    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include "main.h>
+
+
+/**
+ * print_line - prints line on terminal
+ *
+ * @n : no of time '_'is printed
+ *
+ * Return: 0 always - sucess.
+ */
+void print_line(int n)
+{
+int c;
+
+for(c = 1; c <= n; ++c)
+{
+_putchar('_');
+}
+
+_putchar('\n');
+}
