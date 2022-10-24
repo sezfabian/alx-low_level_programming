3f7a8c3b3c6c47dc5db3918d3ebd95c08e02bae3
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-24 09:36:46 +0300

    Update main.h
    be real

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <ctype.h>
 #include <stdio.h>
 int _putchar(char c);
 int _isupper(int c);
@@ -10,5 +6,3 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
-
-#endif
