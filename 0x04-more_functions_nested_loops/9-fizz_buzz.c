2233587e48bb0117906570289f75886c45db9312
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-24 22:01:37 +0300

    Update 9-fizz_buzz.c
    buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@ int main(void)
 {
 int n;
 
-for(n = 1; n <= 100; ++n)
+for(n = 1; n < 100; ++n)
 {
 if (n % 3 == 0)
 {
@@ -25,7 +25,7 @@ printf("%d", n);
 }
 putchar(' ');
 }
-printf("\n");
+printf("Buzz\n");
 
 return (0);
 }
