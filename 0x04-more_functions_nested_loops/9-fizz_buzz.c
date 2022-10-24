ceeec957af148ccf91e1a6e6657e559157126204
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-24 18:15:38 +0300

    Create 9-fizz_buzz.c
    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ */
+int main(void)
+{
+int n;
+for(n = 1; n <= 100; ++n)
+{
+if (n % 3 == 0 && n % 5 == 0 )
+printf( "FizzBuzz");
+else if (n % 3 == 0)
+{
+printf("Fizz");
+}
+else if (n % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%i", n);
+}
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
