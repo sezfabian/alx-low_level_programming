c9b6e4bd6a5910feb7b8d9728c3d74e50fe4894d
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-24 21:57:33 +0300

    Update 9-fizz_buzz.c
    agen fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,19 +9,15 @@ int n;
 
 for(n = 1; n <= 100; ++n)
 {
-if (n % 3 == 0 && n % 5 == 0)
-{
-printf("FizzBuzz");
-}
-else if (n % 3 == 0)
+if (n % 3 == 0)
 {
 printf("Fizz");
 }
-else if (n % 5 == 0)
+if (n % 5 == 0)
 {
 printf("Buzz");
 }
-else
+else if (n % 3 != 0)
 {
 printf("%d", n);
 }
