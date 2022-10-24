ba137d74a543c104c8d72d42d2c7f43355d3e095
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-24 17:03:31 +0300

    Create 8-print_square.c
    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * print_square - prints square using '#' on terminal
+ *
+ * @size : size of square
+ *
+ * Return: 0 always - sucess.
+ */
+void print_square(int size)
+{
+int count;
+int a;
+if (n > 0)
+{
+for (a = 1; a <= size; ++a)
+{
+for (count = 1; count <= size; ++count)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
