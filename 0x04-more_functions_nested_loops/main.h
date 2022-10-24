0bc43f087005b9ad0b1fb56077d545a87aa8c7a2
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-24 09:42:19 +0300

    Update main.h
    endif

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <ctype.h>
 #include <stdio.h>
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -6,3 +11,5 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+
+#endif
