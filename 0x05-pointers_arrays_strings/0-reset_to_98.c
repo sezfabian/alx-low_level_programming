01ba8ef3090933bf30a7312bbc9a8ca40ea8d14a
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-26 00:26:38 +0300

    Create 0-reset_to_98.c
    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * reset_to_98 - resets pointed value to 98
+ *
+ * @*n : integer to be changed
+ *
+ * Return : always 0 - sucess
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
