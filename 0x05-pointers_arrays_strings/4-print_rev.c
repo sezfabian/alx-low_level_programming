74d2831cc2bed049b13a9ad5d0097d7528dcdc3b
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-26 01:48:20 +0300

    Update 4-print_rev.c
    string 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,13 +14,13 @@ void print_rev(char *s)
 int n;
 int count = -1;
 
-for (n = 0; str[n]; n++)
+for (n = 0; s[n]; n++)
 {
 count++;
 }
 for (n = count; n > 0; n--)
 {
-putchar(str[n]);
+putchar(s[n]);
 }
 putchar('\n');
 }
