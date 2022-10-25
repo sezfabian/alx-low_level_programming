7b4b8225334cee03ae0a05b1bbd312aebb5d8c5c
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-26 02:17:33 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,12 +14,16 @@ int n;
 int count = -1;
 int m = 0;
 
+
 for (n = 0; s[n]; n++)
 {
 count++;
-char str[count] = s[count];
 }
-
+char str[count];
+for (n = 0; n <= count; n++)
+{
+str[n] = s[n];
+}
 for (n = count; n >= 0; n--)
 {
 s[m] = str[n];
