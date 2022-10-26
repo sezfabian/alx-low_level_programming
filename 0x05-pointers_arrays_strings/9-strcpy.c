b157e87dd49b9045020ecd4b2081552409d2f9ac
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-27 00:52:22 +0300

    Create 9-strcpy.c
    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+
+/**
+ * *_strcpy - copy pointed string
+ *
+ * @dest : copied string
+ *
+ * @src : input string
+ *
+ * Return : void, correct
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int n = 0;
+
+while (src[n] != '\0')
+{
+dest[n] = src[n];
+n++;
+}
+
+dest[n] = '\0';
+
+return (dest);
+}
