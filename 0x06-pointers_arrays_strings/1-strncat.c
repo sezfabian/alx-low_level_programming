dbe82958c70e00d85b42ed39551ee4f218c4f7a2
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-28 17:48:48 +0300

    Create 1-strncat.c
    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+/**
+ * *_strcat - concatenates two strings for n integers
+ *
+ * @dest: string 1
+ *
+ * @src: string 2
+ *
+ * @n: number of characters to be added
+ *
+ * Return:  returns dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int a;
+int b;
+
+a = 0;
+b = 0;
+
+while (dest[a] != '\0')
+{
+a++;
+}
+while (b < n && src[b] != '\0')
+{
+dest[a] = src[b];
+a++;
+b++;
+}
+dest[a] = '\0';
+return (dest);
+}
