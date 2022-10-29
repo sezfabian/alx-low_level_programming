7b92642cdf0f36e9ddf82aa6ddf96c95e4b4c283
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-30 00:04:06 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 /**
- * cap_string - uppercase each word
+ * *cap_string - caps all first word letter
  * @s1: pointer to string
  *
  * Return: *s1
@@ -14,7 +14,7 @@ for (a = 0; s1[a] != '\0'; a++)
 {
 if (a == 0 || s1[a - 1] == ' ' || s1[a - 1] == '\n' || s1[a - 1] == '\t' || s1[a - 1] == '.')
 {
-if (s1[a] >= 'a' && s1[a] <= 'z')
+if (s1[a] >= 'a') && (s1[a] <= 'z')
 {
 s1[a] -= 32;
 }
