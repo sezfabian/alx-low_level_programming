4c86e9de6ecb8770ac713f8fc26c2f1a43c0692d
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-29 12:04:45 +0300

    Create 5-string_toupper.c
    string to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * string_toupper - lower to upper
+ * @s1: pointer to string
+ * 
+ * Return: *s1 uppercase string.
+ */
+
+char *string_toupper(char *s1)
+{
+int a;
+for (a = 0; s1[a] != '\0'; a++)
+{
+if (s1[a] >= 'a' && s1[a] <= 'z')
+			s1[a] -= 32;
+}
+return (s1);
+}
