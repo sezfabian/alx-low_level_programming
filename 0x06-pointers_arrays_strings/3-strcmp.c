f7e0fb6249dd70d20995b26a18eb4d9b7be3f246
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-29 07:36:14 +0300

    Update 3-strcmp.c
    strcopy

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,17 +10,10 @@ int _strcmp(char *s1, char *s2)
 {
 int a =0;
 int n;
-while(s1[a] != '\0')
+while (s1[a] == s2[a] && (s1[a] != '\0' || s2[a] != '\0'))
 {
-if (s1[a] < s2[a] || s1[a] > s2[a])
-{
+a++;
+}
 n = s1[a] - s2[a];
-break;
-}
-else
-{
-n = 0;
-}
 return (n);
-}
 }
