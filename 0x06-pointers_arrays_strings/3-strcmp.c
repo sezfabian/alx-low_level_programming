5c1196c02f4485af36e78b4c7a7edb3b3f59e57f
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-29 07:42:58 +0300

    Update 3-strcmp.c
    compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,20 +1,20 @@
-#include<stdio.h>
+#include "main.h"
 
 /**
- * *_strncmp - compares srtings
+ * _strncmp - compares srtings
  * @s1 : pointer to  string
  * @s2 : pointer to  string
- * Return: return n
+ * Return: return *n
  */
  
 int _strcmp(char *s1, char *s2)
 {
 int a = 0;
-int n;
+int N;
 while (s1[a] == s2[a] && (s1[a] != '\0' || s2[a] != '\0'))
 {
 a++;
 }
-n = s1[a] - s2[a];
-return (n);
+N = s1[a] - s2[a];
+return (N);
 }
