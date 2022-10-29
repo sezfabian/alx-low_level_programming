f401fcf745e231b2a13d8542ea4039e12f4e67ad
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-10-29 12:09:31 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,7 +3,7 @@
  * string_toupper - lower to upper
  * @s1: pointer to string
  * 
- * Return: *s1 uppercase string.
+ * Return: *s1 uppercase string
  */
 
 char *string_toupper(char *s1)
@@ -14,8 +14,9 @@ for (a = 0; s1[a] != '\0'; a++)
 {
 if (s1[a] >= 'a' && s1[a] <= 'z')
 {
-s1[a] - = 32;
+s1[a] -= 32;
 }
 }
+
 return (s1);
 }
