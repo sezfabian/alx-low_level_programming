0742a5bb770ae4b852354f2b64a14625c780ebba
sezfabian <cheruiyotfabian@gmail.com>
2022-10-29 13:33:43 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * cap_string - uppercase each word
+ * @s1: pointer to string
+ *
+ * Return: *s1
+ */
+char *cap_string(char *s1)
+{
+	int a;
+	
+	for (a = 0; s1[a] != '\0'; a++)
+	{
+		if ((a == 0 || s1[a - 1] == ' ' || s1[a-1] == '\n' || s1[a-1] == '\t') && (s1[a] >= 'a' && s1[a] <= 'z'))
+		{
+			s1[a] -= 32;
+		}
+	}
+	return (s1);
+}
