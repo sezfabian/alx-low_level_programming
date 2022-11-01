6786e2eb285ab34f09fa38f11d2e1b09e484c4cf
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-11-02 02:23:31 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,26 +1,24 @@
-#include<stdio.h>
+#include "main.h"
 
 /**
- * _strchr - locates char in string
- *
- * @s: pointer to string
- * @c: character to be located
- *
+ * _strchr -  function to locates a character in a string.
+ * @s: string with chars
+ * @c: strings with char to check
  * Return: s.
- *
  */
+
 char *_strchr(char *s, char c)
 {
 	int a;
 
-	for (a = 0; s[a] != '\0'; a++)
+	i = 0;
+	while (s[a] != '\0')
 	{
+		a++;
 		if (s[a] == c)
 		{
-			return (&s[a]);
-			break;
+			return (s + a);
 		}
 	}
 	return (0);
 }
-
