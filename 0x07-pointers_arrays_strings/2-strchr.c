354e994e6181a0b3468fe92278e0ba2e183edde0
sezfabian <cheruiyotfabian@gmail.com>
2022-11-01 03:25:28 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+ * _strchr - locates char in string
+ *
+ * @s: pointer to string
+ * @c: character to be located
+ *
+ * Return : *p
+ *
+ */
+char *_strchr(char *s, char c)
+{
+	char *p;
+	int a;
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		if (s[a] == c)
+		{
+			p = &s[a];
+			break;
+		}
+		p = NULL;
+	}
+	return (p);
+}
+
