bea5f70c37e8db9628c4fbef848cc2823da91441
sezfabian <cheruiyotfabian@gmail.com>
2022-11-01 03:03:03 -0700

    nonw

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,8 @@
 void simple_print_buffer(char *buffer, unsigned int size);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 
