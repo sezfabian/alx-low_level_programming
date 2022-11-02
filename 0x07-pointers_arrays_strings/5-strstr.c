bbbb680e37838c86a9a2e8d7df89c51c9c56be31
sezfabian <cheruiyotfabian@gmail.com>
2022-11-02 10:24:38 -0700

    newline

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,24 +10,24 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-int a, b = 0;
-char *st1, *st2, *p;
+	int a, b = 0;
+	char *st1, *st2, *p;
 
-p = 0;
-st1 = haystack;
-st2 = needle;
+	p = 0;
+	st1 = haystack;
+	st2 = needle;
 
-for (a = 0; st1[a] != '\0'; a++)
-{
-if (st1[a] == st2[b])
-{
-b++;
-}
-if (st2[b] == '\0')
-{
-p = (&haystack[a - b + 1]);
-break;
-}
-}
-return (p);
+	for (a = 0; st1[a] != '\0'; a++)
+	{
+		if (st1[a] == st2[b])
+		{
+			b++;
+		}
+		if (st2[b] == '\0')
+		{
+			p = (&haystack[a - b + 1]);
+			break;
+		}
+	}
+	return (p);
 }
