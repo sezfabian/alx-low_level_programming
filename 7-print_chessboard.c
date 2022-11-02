012589093a070cac993effdcb56268406646efc3
sezfabian <cheruiyotfabian@gmail.com>
2022-11-02 13:16:20 -0700

    task 7

---
+++ 7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_chessboard - print multidimensional array
+ * @a: multidim array
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int b, c;
+
+	for (b = 0; b < 8; b++)
+	{
+		for (c = 0; c < 8; c++)
+		{
+			putchar(a[b][c]);
+		}
+		 putchar('\n');
+	}
+}
