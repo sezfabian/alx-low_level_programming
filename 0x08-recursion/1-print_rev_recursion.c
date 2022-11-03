8f2eb592b122f47e81764c05d4df10137ae22fda
sezfabian <cheruiyotfabian@gmail.com>
2022-11-03 01:45:08 -0700

    recurssion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ *
+ * _puts_recursion - prints a string, followed by a new line.
+ *
+ * @s: pointer to string.
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	printf("%s\n", s);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+# Welcome to recursion, more like a function that calls itself untill the functions output fulfills the functions function.
