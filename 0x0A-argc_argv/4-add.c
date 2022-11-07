95ca377223072d84fab667a090b025a863078341
sezfabian <cheruiyotfabian@gmail.com>
2022-11-07 13:14:12 -0800

    4 add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * main - adds numbers passed
+ *
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b, i, sum = 0;
+
+	if (argc < 3)
+	{
+		printf("%i\n", 0);
+		return (0);
+	}
+
+	else
+	{
+		for (a = 1; a < argc; a++)
+		{
+			for (i = 0; argv[a][i] != '\0'; i++)
+			{
+				if (!(isdigit(argv[a][i])))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+			b = atoi(argv[a]);
+			sum = sum + b;
+		}
+		printf("%i\n", sum);
+		return (sum);
+	}
+}
+
