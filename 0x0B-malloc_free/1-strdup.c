e10f066f1b9ce7e3183b8077fb9ab36bb852d816
sezfabian <cheruiyotfabian@gmail.com>
2022-11-10 14:18:59 -0800

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * _strdup - creates a copy of string in new memory
+ *
+ * @str: pointer to string
+ *
+ * Return: pointer to arr.
+ */
+
+char *_strdup(char *str)
+{
+	char *arr;
+	int l;
+
+	if (str == NULL)
+		return (0);
+
+	for (l = 0; str[l] != '\0'; l++)
+		;
+
+	l = l + 1;
+	arr = malloc((l) * sizeof(char));
+
+	while (l--)
+		arr[l] = str[l];
+	return (arr);
+}
+
