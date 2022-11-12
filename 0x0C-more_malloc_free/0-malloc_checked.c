24188408bd5bfecfc1889033748f9f110a638d44
sezfabian <cheruiyotfabian@gmail.com>
2022-11-12 11:49:00 -0800

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked -  that allocates memory using malloc
+ *
+ * @b: size of memory to be allocated
+ *
+ * Return: pointer to heap.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+
+	int *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == 0)
+	{
+		exit(98);
+	}
+
+	return (ptr);
+}
+
