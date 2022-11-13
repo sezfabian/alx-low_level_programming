947e089410d59cac604b5794c864971f28299efa
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-11-13 06:15:01 +0300

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -18,7 +18,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	arr = malloc((max - min) + 1);
+	arr = malloc(sizeof(int) * ((max - min) + 1));
 
 	if (arr == NULL)
 		return (NULL);
