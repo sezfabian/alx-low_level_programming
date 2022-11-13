dd295acb737c9ea5b9c1234c177d2d4bcc978f55
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-11-13 04:59:47 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,8 +15,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *str;
 	unsigned int a, b, c;
 
-	for(c = 0; s1[c] != '\n'; c++)
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for(c = 0; s1[c] != '\0'; c++)
 		;
+	for (b = 0; s2[b] != '\0'; b++)
+		;
+
+	if (n > b)
+		n = b;
 
 	str = malloc(c + n + 1);
 
@@ -25,7 +36,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return(0);
 	}
 
-	for(a = 0; a < c + n; a++)
+	for(a = 0; a < (c + n); a++)
 	{
 		if (a < c)
 			str[a] = s1[a];
