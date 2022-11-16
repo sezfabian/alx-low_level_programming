9ed4a1c0a55583879225a785e1f3e3ff494edf89
sezfabian <55694289+sezfabian@users.noreply.github.com>
2022-11-16 05:31:03 +0300

    Update dog.h
    betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,13 @@
 /**
- * dog - structure dog
+ * struct dog - structure dog
  * @name: attribute pointer to char name in dog
  * @age: attribute float age
  * @owner: attribute pointer to char owner in dog
  */
- 
+
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
-void init_dog(struct dog *d, char *name, float age, char *owner);
