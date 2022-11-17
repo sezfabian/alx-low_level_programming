d4461e7c13caa12b210fe82154e3efaecda4d4b0
sezfabian <cheruiyotfabian@gmail>
2022-11-17 03:45:16 +0300

    task 4

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
 #ifndef FILE_ABS
 #define FILE_ABS
 
-#define ABS(X) (X * ((X >= 0) - (X < 0)))
+#define ABS(X) ((X) * (((X) >= 0) - ((X) < 0)))
 
 #endif /*ABS*/
 
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef FILE_SUM
+#define FILE_SUMi
+
+#define SUM(x, y) ((x) + (y))
+
+#endif /*SUM*/
