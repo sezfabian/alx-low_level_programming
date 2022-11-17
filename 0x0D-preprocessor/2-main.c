a5db66aa22c25fd0093669a84fbae081dc7db0b7
sezfabian <cheruiyotfabian@gmail>
2022-11-17 03:06:22 +0300

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * Main - print file compiled from
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
