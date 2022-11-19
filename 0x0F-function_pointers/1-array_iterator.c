c25c29e74a9de194a2bf81fff4fe0a816b45606b
Sedge <sedge@localhost.localdomain>
2022-11-19 15:35:32 +0300

    task1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,10 +12,10 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int a;
-	
+
 	if (!array || !action)
 		return;
-	
+
 	for (a = 0; a < size; a++)
 	{
 		action(array[a]);
