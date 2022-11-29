0c520792fce2ec2530c0e4f1e36109372b062483
sezfabian <cheruiyotfabian@gmail.com>
2022-11-30 01:05:08 +0300

    print before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * before_main - prints before main is excecuted
+ * Return: void
+ */
+
+void before_main(void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n ");
+}
+
