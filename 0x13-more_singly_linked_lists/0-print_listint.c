81825c4402d1b7aa955e20ab15bac2c183e44ddc
sezfabian <cheruiyotfabian@gmail.com>
2023-01-09 11:46:45 +0300

    tasks 01

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -20,5 +20,5 @@ size_t print_listint(const listint_t *h)
 		h = h->next;
 		n++;
 	}
-	return (c);
+	return (n);
 }
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -19,6 +19,6 @@ size_t listint_len(const listint_t *h)
 		h++;
 		h = h->next;
 	}
-	return (count);
+	return (n);
 }
 
