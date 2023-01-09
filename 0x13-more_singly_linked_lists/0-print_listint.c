5e7c4843cc5dcf945138bd1243365c055bbad989
Fabian Cheruiyot <55694289+sezfabian@users.noreply.github.com>
2023-01-09 08:11:50 +0000

    Update 0-print_listint.c
    real

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,16 +9,16 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t n = 0;
 
 	if (h == NULL)
 		return (0);
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
+		printf("%i\n", h->n);
 		h = h->next;
-		count++;
+		n++;
 	}
-	return (count);
+	return (c);
 }
