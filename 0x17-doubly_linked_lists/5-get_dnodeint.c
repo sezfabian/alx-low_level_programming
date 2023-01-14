c72563c6b34c79baee7744c1a7635f7367ae46c0
sezfabian <cheruiyotfabian@gmail.com>
2023-01-14 11:29:08 +0300

    task 05dl

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns node at index
+ * @head: pointer to pointer to head of list
+ * @index: node to be accessed
+ * Return: node at index, NULL if does not exist
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	if (head)
+	{
+		unsigned int count = 0;
+
+		while (count < index && head != NULL)
+		{
+			head = head->next;
+			count++;
+		}
+			if (head != NULL)
+			return (head);
+	}
+	else
+		return (NULL);
+}
+
