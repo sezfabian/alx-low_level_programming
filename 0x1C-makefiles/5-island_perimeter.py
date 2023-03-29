69bc37eca8029e1178611e4feeeae243cbd0a0ce
sezfabian <cheruiyotfabian@gmail.com>
2023-03-29 19:09:49 +0300

    perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,8 +10,8 @@ def island_perimeter(grid):
     """
     length = 0
     width = 0
-    for i in range(0, len(grid) - 1):
-        for j in range(0, len(grid[1])-1):
+    for i in range(0, len(grid)):
+        for j in range(0, len(grid[1])):
             if grid[i][j] == 1 and grid[i-1][j] == 0:
                 width += 1
             if grid[i][j] == 1 and grid[i][j-1] == 0:
